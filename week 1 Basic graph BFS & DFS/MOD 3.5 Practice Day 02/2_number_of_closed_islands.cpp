69eac9b3e9b0c9f931c0053660e70868767a250c
Mohammad Abdullaha Al Forhad <119785564+foorhad@users.noreply.github.com>
2024-09-30 22:39:11 +0600

    Update 2_number_of_closed_islands.cpp

--- week 1 Basic graph BFS & DFS/MOD 3.5 Practice Day 02/2_number_of_closed_islands.cpp
+++ week 1 Basic graph BFS & DFS/MOD 3.5 Practice Day 02/2_number_of_closed_islands.cpp
@@ -1,55 +1,52 @@
+#include <bits/stdc++.h>
 class Solution {
 public:
-    int cnt=0;
-    bool vis[1005][1005]={false};
-    int flag=0;
+    bool flag=true;
+    int island = 0;
+    int row,col;
+    bool vis[110][110];
+    vector<pair<int,int>> movem = { {0,1}, {0,-1}, {-1,0}, {1,0} };
 
-    bool valid(int i, int j,int row, int col){
-    if (i >= 0 && i<row && j>=0 && j<col)return true;
-    else return false;
+    bool valid(int i, int j){
+        if(i>=0 && i<row && j>=0 && j<col)return true;
+        return false;
     }
-
-    void bfs(vector<vector<int>>& grid,int si, int sj){
-        int row = grid.size();
-        int col = grid[0].size();
-        
-        vector<pair<int, int>> xy = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
-
+    void dfs(int i, int j, vector<vector<int>>&grid){
+        vis[i][j]=true;
         queue<pair<int,int>>q;
-        q.push({si,sj});
-        vis[si][sj]=true;
+        q.push({i,j});
 
         while(!q.empty()){
-            pair<int,int>node=q.front(); q.pop();
-            int r= node.first;
-            int c= node.second;
+            pair<int,int> parent = q.front();
+            q.pop();
+            for(auto child : movem){
+                int child_i = child.first + parent.first;
+                int child_j = child.second + parent.second;
 
-            for(auto child : xy){
-                int i=r+child.first;
-                int j=c+child.second;
-
-                if(!valid(i,j,row,col)) flag=1;
-            
-                if(valid(i,j,row,col) && !vis[i][j] && grid[i][j]==0){
-                    q.push({i,j});
-                    vis[i][j]=true;
+                if(!valid(child_i,child_j))flag=false;
+                
+                if(valid(child_i,child_j) && !vis[child_i][child_j] && grid[child_i][child_j]==0){
+                    q.push({child_i,child_j});
+                    vis[child_i][child_j]=true;
                 }
             }
         }
-        if(flag == 1)cnt--;
-        flag=0;
     }
+
     int closedIsland(vector<vector<int>>& grid) {
-        int row = grid.size();
-        int col = grid[0].size();
-        for(int i=0; i<row; i++){
-            for(int j=0; j<col; j++){
-                if(grid[i][j]==0 && !vis[i][j]){
-                    cnt++;
-                    bfs(grid,i,j);
+        memset(vis,false,sizeof(vis));
+        row = grid.size();
+        col = grid[0].size();
+
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(!vis[i][j] && grid[i][j]==0){
+                    dfs(i,j,grid);
+                    if(flag)island++;
+                    flag = true;
                 }
             }
         }
-    return cnt;  
+    return island;
     }
 };
