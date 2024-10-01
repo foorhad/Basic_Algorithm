e8ec9716b5f7a67e7b8c684bfa64c9d9bf5507bb
Mohammad Abdullaha Al Forhad <119785564+foorhad@users.noreply.github.com>
2024-10-01 10:50:00 +0600

    Create 1.3_connected_or_not_using_adj_matrix.cpp

---
+++ week 1 Basic graph BFS & DFS/MOD 4 Assignment 01/1.3_connected_or_not_using_adj_matrix.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    int matrix[1005][1005];
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < e; j++)
+        {
+            matrix[i][j] = -1;
+            if (i == j)
+                matrix[i][j] = 1;
+        }
+    }
+
+    while (e--)
+    {
+        int a, b;
+        cin >> a >> b;
+        matrix[a][b] = 1;
+    }
+
+    long long int q;
+    cin >> q;
+    while (q--)
+    {
+        int u, v;
+        cin >> u >> v;
+
+        if (matrix[u][v] == 1)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
