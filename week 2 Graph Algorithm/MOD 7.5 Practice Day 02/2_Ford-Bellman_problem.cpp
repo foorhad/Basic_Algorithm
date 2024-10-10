bca83416d1c3e39bd16dfe848103f80fa0d20924
Mohammad Abdullaha Al Forhad <119785564+foorhad@users.noreply.github.com>
2024-10-10 17:15:29 +0600

    Update 2_Ford-Bellman_problem.cpp

--- week 2 Graph Algorithm/MOD 7.5 Practice Day 02/2_Ford-Bellman_problem.cpp
+++ week 2 Graph Algorithm/MOD 7.5 Practice Day 02/2_Ford-Bellman_problem.cpp
@@ -1,42 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
-class Edge{
-public: 
-    int u,v,c;
-    Edge(int u, int v, int c){
-        this->u=u;
-        this->v=v;
-        this->c=c;
+const int N = 1e5 + 5;
+
+class Edges
+{
+public:
+    int u, v, c;
+    Edges(int u, int v, int c)
+    {
+        this->u = u;
+        this->v = v;
+        this->c = c;
     }
 };
-
-int main() {
+int main()
+{
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    int n,m;
-    cin>>n>>m;
-    int e = m;
-    vector<Edge> edgelist;
 
-    while(m--){
-        int u,v,c;cin>>u>>v>>c;
-        edgelist.push_back(Edge(u,v,c));
+    int n, m;
+    cin >> n >> m;
+    int dis[N];
+    vector<Edges> edgelist;
+    while (m--)
+    {
+        int u, v, c;
+        cin >> u >> v >> c;
+        edgelist.push_back(Edges(u, v, c));
     }
-    
-    int dis[n+1];
-    for(int i=1; i<=n; i++)dis[i]=30000;
+    for (int i = 1; i <= n; i++)
+        dis[i] =  30000;
     dis[1]=0;
-    for(int i=1; i<n;i++){
-        for(auto child : edgelist){
-            int u = child.u;
-            int v = child.v;
-            int c = child.c;
-            if(dis[u] < 30000 && dis[u]+c < dis[v]){
-                dis[v] = dis[u]+c;
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (Edges n : edgelist)
+        {
+            if(dis[n.u]!= 30000 && dis[n.u]+n.c < dis[n.v]){
+                dis[n.v] = dis[n.u]+n.c;
             }
         }
     }
-    if(e!=0)for(int i=1; i<=n; i++)cout<<dis[i]<<" ";
-    
+    for(int i=1;i<=n;i++)cout<<dis[i]<<" ";
 }
