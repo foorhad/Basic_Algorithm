2bea89e494078b5165b79854322502727cd1f650
Mohammad Abdullaha Al Forhad <119785564+foorhad@users.noreply.github.com>
2024-10-10 19:47:52 +0600

    Update 3_Floyd.cpp

--- week 2 Graph Algorithm/MOD 7.5 Practice Day 02/3_Floyd.cpp
+++ week 2 Graph Algorithm/MOD 7.5 Practice Day 02/3_Floyd.cpp
@@ -1,52 +1,50 @@
 #include <bits/stdc++.h>
+#define ll long long int
+#define pi pair<ll, ll>
+#define nl '\n'
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+const long long int INF = 1e18 + 5;
+const int N = 1e6 + 5;
 using namespace std;
 
 int main()
 {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-
+    fast();
     int n;
     cin >> n;
-    int matrix[105][105];
+    int grid[n + 1][n + 1];
     for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j <= n; j++)
         {
-            cin >> matrix[i][j];
-            if (matrix[i][j] == -1)
-                matrix[i][j] = INT_MAX;
+            cin >> grid[i][j];
+            if(grid[i][j]==-1)grid[i][j]=INT_MAX;
         }
     }
-
     for (int k = 1; k <= n; k++)
     {
         for (int i = 1; i <= n; i++)
         {
             for (int j = 1; j <= n; j++)
             {
-                if (matrix[i][k] != INT_MAX && matrix[k][j] != INT_MAX && matrix[i][k] + matrix[k][j] < matrix[i][j])
+                if (grid[i][k]!=INT_MAX && grid[k][j]!= INT_MAX && grid[i][k] + grid[k][j] < grid[i][j])
                 {
-                    matrix[i][j] = matrix[i][k] + matrix[k][j];
+                    grid[i][j] = grid[i][k] + grid[k][j];
                 }
             }
         }
     }
-
-    int mx = INT_MIN;
+    int max_dis = INT_MIN;
     for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j <= n; j++)
         {
-            if (matrix[i][j] > mx)
+            if (grid[i][j] > max_dis && grid[i][j]!=INT_MAX)
             {
-                if (matrix[i][j] != INT_MAX)
-                {
-                    mx = matrix[i][j];
-                }
+            max_dis = grid[i][j];
             }
         }
     }
-    cout << mx;
+    cout << max_dis << nl;
+    return 0;
 }
