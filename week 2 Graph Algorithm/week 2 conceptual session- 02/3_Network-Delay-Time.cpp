5b059e4dbe80b676824a75f34e03335f34ea1698
Mohammad Abdullaha Al Forhad <119785564+foorhad@users.noreply.github.com>
2024-10-11 19:21:51 +0600

    Update 3_Network-Delay-Time.cpp

--- week 2 Graph Algorithm/week 2 conceptual session- 02/3_Network-Delay-Time.cpp
+++ week 2 Graph Algorithm/week 2 conceptual session- 02/3_Network-Delay-Time.cpp
@@ -1,54 +1,42 @@
-class Solution
-{
+class Solution {
 public:
-    vector<pair<int, int>> adj[105];
-    int dis[105];
-    void dijktre(int src)
-    {
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        pq.push({0, src});
-        dis[src] = 0;
+    vector<pair<int,int>>adj_list[105];
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {    
+        for(auto val : times){
+            int u = val[0];
+            int v = val[1];
+            int w = val[2];
+            adj_list[u].push_back({w,v});
+        }
+        int dis[105];
+        for(int i=1;i<=n;i++)dis[i]=INT_MAX;
 
-        while (!pq.empty())
-        {
-            pair<int, int> p = pq.top();
-            pq.pop();
-            int parentCost = p.first;
-            int parentNode = p.second;
+        dis[k]=0;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        pq.push({0,k});
 
-            for (auto p : adj[parentNode])
-            {
-                int childNode = p.second;
-                int childCost = p.first;
-
-                if (parentCost + childCost < dis[childNode])
-                {
-                    dis[childNode] = parentCost + childCost;
-                    pq.push({dis[childNode], childNode});
+        while(!pq.empty()){
+            pair<int,int> p = pq.top();pq.pop();
+            int parent = p.second;
+        
+            for(auto c : adj_list[parent]){
+                int child = c.second;
+                int child_cost = c.first;
+                if(dis[parent]+child_cost < dis[child]){
+                    dis[child] = dis[parent]+child_cost;
+                    pq.push({dis[child],child}); 
                 }
             }
         }
-    }
-    int networkDelayTime(vector<vector<int>> &times, int n, int k)
-    {
-        for (auto x : times)
-        {
-            int u = x[0];
-            int v = x[1];
-            int w = x[2];
-            adj[u].push_back({w, v});
+        int max_val = INT_MIN;
+        bool flag = true;
+        for(int i=1;i<=n;i++){
+            if(dis[i]==INT_MAX)flag=false;
+            if(dis[i]>max_val && dis[i]!=INT_MAX){
+                max_val = dis[i];
+            }
         }
-        for (int i = 1; i <= n; i++)
-            dis[i] = INT_MAX;
-        dijktre(k);
-
-        int mx = 0;
-        for (int i = 1; i <= n; i++)
-        {
-            if (dis[i] == INT_MAX)
-                return -1;
-            mx = max(mx, dis[i]);
-        }
-        return mx;
+    if(flag)return  max_val;
+    return -1;
     }
 };
