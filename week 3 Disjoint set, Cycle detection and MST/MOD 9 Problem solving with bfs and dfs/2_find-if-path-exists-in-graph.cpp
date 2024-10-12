e401998e69b9ae32ed3519cb4cb97535e9646b50
Mohammad Abdullaha Al Forhad <119785564+foorhad@users.noreply.github.com>
2024-10-12 12:55:38 +0600

    Update 2_find-if-path-exists-in-graph.cpp

--- week 3 Disjoint set, Cycle detection and MST/MOD 9 Problem solving with bfs and dfs/2_find-if-path-exists-in-graph.cpp
+++ week 3 Disjoint set, Cycle detection and MST/MOD 9 Problem solving with bfs and dfs/2_find-if-path-exists-in-graph.cpp
@@ -1,40 +1,27 @@
-class Solution
-{
+class Solution {
 public:
-    vector<int> adj_list[200005];
-    bool vis[200005];
-    bool dis[200005];
-
-    void bfs(int i)
-    {
-        queue<int> q;
-        q.push(i);
-        vis[i] = true;
-        while (!q.empty())
-        {
-            int parent = q.front();
-            q.pop();
-            for (auto child : adj_list[parent])
-            {
-                if (!vis[child])
-                {
-                    q.push(child);
-                    vis[child] = true;
-                }
+    int r,c;
+    vector<int>adj_list[200020];
+    bool vis[200020];
+    void dfs(int s){
+        vis[s]=true;
+        for(int val : adj_list[s]){
+            if(!vis[val]){
+                dfs(val);
             }
         }
     }
-    bool validPath(int n, vector<vector<int>> &edges, int source, int destination)
-    {
-        for (auto vec : edges)
-        {
-            int u = vec[0];
-            int v = vec[1];
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+        memset(vis,false,sizeof(vis));
+        for(auto val : edges){
+            int u = val[0];
+            int v = val[1];
             adj_list[u].push_back(v);
             adj_list[v].push_back(u);
         }
+        dfs(source);
 
-        bfs(source);
-        return vis[destination];
+
+    return vis[destination];
     }
 };
